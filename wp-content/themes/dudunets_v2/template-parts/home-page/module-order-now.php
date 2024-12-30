828fe7be220feb4067bb5b834fed7d08e50a5da9
wycliffe ottawa <ottawawycliffe@gmail.com>
2024-12-30 23:58:18 +0300

    worked on quality lifestyle video on fron page, worked on module order now module on front page

--- wp-content/themes/dudunets_v2/template-parts/home-page/module-order-now.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/module-order-now.php
@@ -1,16 +1,19 @@
 <?php
+$the_post = get_post(750);
+$image = get_post_thumbnail($the_post->ID);
+$cta = get_field("cta",$the_post->ID);
 ?>
 <section class="module module--order-now">
     <div>
-        <div class="relative bg-black w-full h-[500px] bg-no-repeat bg-cover bg-fixed bg-center" style="background-image: url(http://localhost/dudunets/wp-content/uploads/2024/08/product_screens_bkg.jpg);">
+        <div class="relative bg-black w-full h-[500px] bg-no-repeat bg-cover bg-fixed bg-center" style="background-image: url(<?php echo $image['image'][0]?>);">
             <div class="relative h-full">
                 <div class="absolute inset-0 w-full top-[25%]">
                     <div class="container mx-auto text-white">
                         <div class="w-4/12 pl-10">
-                            <h2 class="text-3xl font-400">Our retractable window and door screens</h2>
-                            <p>offer smooth operation, seamless design, and lasting durability—welcoming fresh air while keeping bugs out!</p>
-                            <a href="#" class="inline-flex gap-3 items-center mt-6 font-semibold bg-primary p-2 text-secondary px-8 py-3 hover:bg-secondary hover:text-primary transition-all duration-100 delay-200">
-                                <span>Order now</span>
+                            <h2 class="text-3xl font-400"><?php echo $the_post->post_title?></h2>
+                            <p><?php echo $the_post->post_excerpt?></p>
+                            <a href="<?php echo $cta['url']?>" class="inline-flex gap-3 items-center mt-6 font-semibold bg-primary p-2 text-secondary px-8 py-3 hover:bg-secondary hover:text-primary transition-all duration-100 delay-200">
+                                <span><?php echo $cta['title']?></span>
                                 <span>
                                     <svg class="w-6 h-6 fill-current"><use xlink:href="#icon-right"></use>
                                     </svg>
