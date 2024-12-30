920196629af42a789634a0ebacaba3cf472a0d1a
denni savai <dennis.savai316@gmail.com>
2024-12-30 11:31:31 +0300

    added homepage sections

--- wp-content/themes/dudunets_v2/template-parts/home-page/module-order-now.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/module-order-now.php
@@ -1,9 +1,25 @@
 <?php
 ?>
-<div class="module-order-now">
-<h3>
-    Our Retractable window and door screens
-    <small>Offer Smooth Operation, Seamless Design and Lasting Durability</small>
-</h3>
-    <a href="#"> Order Now</a>
-</div>
+<section class="module module--order-now">
+    <div>
+        <div class="relative bg-black w-full h-[500px] bg-no-repeat bg-cover bg-fixed bg-center" style="background-image: url(http://localhost/dudunets/wp-content/uploads/2024/08/product_screens_bkg.jpg);">
+            <div class="relative h-full">
+                <div class="absolute inset-0 w-full top-[25%]">
+                    <div class="container mx-auto text-white">
+                        <div class="w-4/12 pl-10">
+                            <h2 class="text-3xl font-400">Our retractable window and door screens</h2>
+                            <p>offer smooth operation, seamless design, and lasting durability—welcoming fresh air while keeping bugs out!</p>
+                            <a href="#" class="inline-flex gap-3 items-center mt-6 font-semibold bg-primary p-2 text-secondary px-8 py-3 hover:bg-secondary hover:text-primary transition-all duration-100 delay-200">
+                                <span>Order now</span>
+                                <span>
+                                    <svg class="w-6 h-6 fill-current"><use xlink:href="#icon-right"></use>
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- wp-content/themes/dudunets_v2/template-parts/home-page/module-quality-lifestyle.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/module-quality-lifestyle.php
@@ -7,6 +7,21 @@
                 <h2 class="text-3xl font-400 mb-1">Experience luxury and quality lifestyle</h2>
                 <p class="font-quicksand text-gray font-bold">Our window and door screen keep bugs out and fresh air in for your maximum confort</p>
             </div>
+            <div class="relative cursor-default pb-6 lg:pb-0">
+                <a href="https://www.youtube.com/embed/ZXjHqf9oCFU?si=-z5Ns4Zw5MAeiAsj" data-fancybox-type="iframe" class="fancy_youtube fancybox iframe relative cursor-default block">
+                    <div class="absolute inset-0 bg-black/10 w-full h-[500px] overflow-hidden"></div>
+                    <div class="absolute z-10 inset-0 flex justify-center items-center">
+                        <div class="bg-white text-red w-24 h-24 rounded-full flex justify-center items-center shadow-3xl hover:bg-primary hover:text-white cursor-pointer">
+                            <svg class="w-10 h-10 fill-current">
+                                <use xlink:href="#icon-play"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="w-full h-[500px] overflow-hidden">
+                        <img src="http://localhost/dudunets/wp-content/uploads/2024/08/Untitled-design-60.png" alt="Side view of a balcony door with a retractable screen fully extended. The screen features a fine mesh material and a discreet roller mechanism for smooth operation" class="w-full h-full object-cover">
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
