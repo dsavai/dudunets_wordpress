444d80b29ebfde2c578d0fcf8c947de6c1d05a0b
denni savai <dennis.savai316@gmail.com>
2024-12-31 11:07:52 +0300

    added the installations page

--- wp-content/themes/dudunets_v2/page-categories.php
+++ wp-content/themes/dudunets_v2/page-categories.php
@@ -5,24 +5,30 @@
 get_header();
 ?>
 
-    <div class="categories-list">
-        <h1>Categories</h1>
-        <ul>
-            <?php
-            $categories = get_categories(); // Fetch all categories
-            if ( $categories ) {
-                foreach ( $categories as $category ) {
-                    echo '<li>';
-                    echo '<a href="' . get_category_link( $category->term_id ) . '">' . esc_html( $category->name ) . '</a>';
-                    echo ' (' . esc_html( $category->count ) . ')'; // Display post count
-                    echo '</li>';
+    <main id="mainMain" class="module">
+        <div class="px-4 lg:px-0 lg:w-[700px] mx-[auto]">
+            <div class="my-8">
+                <div class="flex flex-col justify-center items-center text-center">
+                    <h2 class="mb-2 text-3xl font-400">Categories</h2>
+                    <p class="text-black/60 font-medium leading-relaxed">Lorem ipsum dolor sit amet consectetur. Fermentum feugiat risus ac tristique pharetra at leo vitae.</p>
+                </div>
+            </div>
+            <ul class="flex gap-3 flex-wrap justify-center">
+                <?php
+                $categories = get_categories(); // Fetch all categories
+                if ( $categories ) {
+                    foreach ( $categories as $category ) {
+                        echo '<li>';
+                        echo '<a class="flex justify-center items-center border text-sm font-semibold text-secondary  border-primary bg-primary/10 mb-2 rounded-lg px-3 py-1" href="' . get_category_link( $category->term_id ) . '">' . esc_html( $category->name ) . '<span class="flex justify-center items-center text-xs border border-black/10 bg-secondary text-white ml-4 rounded-full w-4 h-4">'.esc_html( $category->count ).'</span>' .'</a>';
+                        echo '</li>';
+                    }
+                } else {
+                    echo '<p>No categories found.</p>';
                 }
-            } else {
-                echo '<p>No categories found.</p>';
-            }
-            ?>
-        </ul>
-    </div>
+                ?>
+            </ul>
+        </div>
+    </main>
 
 <?php
 get_footer();
--- wp-content/themes/dudunets_v2/page-installations.php
+++ wp-content/themes/dudunets_v2/page-installations.php
@@ -7,8 +7,143 @@ get_header();
             <h2 class="mb-2 text-3xl font-400">Our installations</h2>
             <p class="text-black/60 font-medium leading-relaxed">Lorem ipsum dolor sit amet consectetur. Fermentum feugiat risus ac tristique pharetra at leo vitae.</p>
         </div>
-        <div class="section--videos mt-16">
-            
+        <div class="mb-4">
+            <form>
+                <select class="w-[200px] h-[45px] border-[1px] border-secondary/10 py-2 px-5 rounded-lg text-sm">
+                    <option value="">Doors</option>
+                    <option value="">Window</option>
+                    <option value="">Balcony</option>
+                    <option value="">Screens</option>
+                </select>
+            </form>
+        </div>
+        <div class="section--installations">
+            <div class="grid grid-cols-2 gap-x-6 gap-y-10">
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#">
+                        <div class="h-[400px] overflow-hidden">
+                            <img src="https://picsum.photos/600/600" alt="" class="object-cover w-full h-full" />
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-center gap-3 w-10/12">
+                                <span>
+                                    <svg class="w-5 h-5 fill-current">
+                                        <use xlink:href="#icon-location"></use>
+                                    </svg>
+                                </span>
+                                <h3 class="font-quicksand font-bold text-lg truncate">Nyali Beach Hotel, Mombasa county</h3>
+                            </div>
+                            <div class="bg-black/10 h-[1px] w-full my-2"></div>
+                            <p class="text-black/60 font-semibold leading-5 truncate">Keep your home cool and shaded without getting up from the comfort of your seat.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="mt-12 flex justify-center">
+                <a href="#" class="load-more-btn flex gap-3 items-center font-semibold bg-primary p-2 text-secondary px-8 py-3 hover:bg-secondary hover:text-primary transition-all duration-100 delay-200">
+                    <span>Load more</span>
+                    <span>
+                        <svg class="w-8 h-8 fill-current">
+                            <use xlink:href="#icon-menudots"></use>
+                        </svg>
+                    </span>
+                </a>
+            </div>
         </div>
     </div>
 </main>
