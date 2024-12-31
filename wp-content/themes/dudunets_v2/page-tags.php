45b5084ad5e5ecc26fe76c52924d4eba3e402e8b
denni savai <dennis.savai316@gmail.com>
2024-12-31 10:02:16 +0300

    styled the tags page

--- wp-content/themes/dudunets_v2/page-tags.php
+++ wp-content/themes/dudunets_v2/page-tags.php
@@ -5,14 +5,20 @@
 get_header();
 ?>
 
-    <div class="tags-list">
-        <h1>Tags</h1>
-        <ul>
+<main id="mainMain" class="module">
+    <div class="px-4 lg:px-0 lg:w-[700px] mx-[auto]">
+        <div class="my-8">
+            <div class="flex flex-col justify-center items-center text-center">
+                <h2 class="mb-2 text-3xl font-400">Tags</h2>
+                <p class="text-black/60 font-medium leading-relaxed">Lorem ipsum dolor sit amet consectetur. Fermentum feugiat risus ac tristique pharetra at leo vitae.</p>
+            </div>
+        </div>
+        <ul class="flex gap-3 flex-wrap justify-center">
             <?php
             $tags = get_tags();
             if ( $tags ) {
                 foreach ( $tags as $tag ) {
-                    echo '<li><a href="' . get_tag_link( $tag->term_id ) . '">' . esc_html( $tag->name ) . '</a> (' . esc_html( $tag->count ) . ')</li>';
+                    echo '<li><a  class="flex justify-center items-center border text-sm font-semibold text-secondary  border-primary bg-primary/10 mb-2 rounded-lg px-3 py-1" href="' . get_tag_link( $tag->term_id ) . '">'. esc_html( $tag->name ) . '<span class="flex justify-center items-center text-xs border border-black/10 bg-secondary text-white ml-4 rounded-full w-4 h-4">'.esc_html( $tag->count ).'</span>' .' </a> </li>';
                 }
             } else {
                 echo '<p>No tags found.</p>';
@@ -20,6 +26,7 @@ get_header();
             ?>
         </ul>
     </div>
+</main>
 
 <?php
 get_footer();
