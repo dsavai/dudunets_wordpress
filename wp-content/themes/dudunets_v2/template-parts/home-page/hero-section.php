8294bdd894479575bb7718e955b64db25ad63d3d
denni savai <dennis.savai316@gmail.com>
2025-01-01 10:33:04 +0300

    added the installations page

--- wp-content/themes/dudunets_v2/template-parts/home-page/hero-section.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/hero-section.php
@@ -13,7 +13,11 @@ $sliders = get_custom_posts_by_slug("front_page_slider","ASC");
                     $image = get_post_thumbnail($slider->ID);
                     ?>
                     <div class="swiper-slide relative">
-                        <div class="bg-black absolute inset-0 bg-opacity-20 z-10"></div>
+                        <div class="absolute bottom-14 inset-x-0 text-center z-[99] text-white">
+                            <h1 class="text-5xl font-quicksand font-bold mb-2">Retractable door screens</h1>
+                            <p>Building a better world, together, we can make a difference</p>
+                        </div>
+                        <div class="absolute inset-0 w-full h-full z-10 bg-gradient-to-b from-black to-black-200 rotate-180 opacity-80"></div>
                         <img src="<?php echo $image['image'][0]?>" alt="<?php echo $image["alt"]?>" class="block w-full h-full object-cover object-center aspect-square" />
                     </div>
                 <?php endforeach;?>
