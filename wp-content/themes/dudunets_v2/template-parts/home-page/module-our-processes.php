07d8080cae555a2fca9280bce4ddcb2a187cc5e2
wycliffe ottawa <ottawawycliffe@gmail.com>
2025-01-15 23:10:22 +0300

    changes to process section

--- wp-content/themes/dudunets_v2/template-parts/home-page/module-our-processes.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/module-our-processes.php
@@ -8,8 +8,8 @@ $cta = get_field("cta",$post->ID);
     <div class="bg-gray-feather flex flex-col justify-center items-center border-t border-black/10">
         <div class="py-6 px-4 lg:py-24 lg:px-24">
             <div class="mx-auto px-4 lg:px-0 lg:w-[800px] text-center">
-                <h2 class="mb-2 text-3xl font-400">Our process</h2>
-                <p class="text-black/60 font-medium leading-relaxed">Lorem ipsum Innovation thrives on the cusp of creativity and necessity, and today's world presents. creativity and necessity, and today's world presents</p>
+                <h2 class="mb-2 text-3xl font-400"><?php echo $post->post_title?></h2>
+                <p class="text-black/60 font-medium leading-relaxed"><?php echo $post->post_content?></p>
                 <div class="mt-5">
                     <a href="<?php echo $cta['url']?>" class="inline-flex gap-3 items-center font-semibold bg-primary p-2 text-secondary px-8 py-3 hover:bg-secondary hover:text-primary transition-all duration-100 delay-200">
                         <span><?php echo $cta['title']?></span>
