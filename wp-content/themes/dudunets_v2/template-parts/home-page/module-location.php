056b21c63f4773b60f9a03dfc9ddf4482f1e4530
denni savai <dennis.savai316@gmail.com>
2025-10-27 23:02:56 +0300

    added files

--- wp-content/themes/dudunets_v2/template-parts/home-page/module-location.php
+++ wp-content/themes/dudunets_v2/template-parts/home-page/module-location.php
@@ -2,8 +2,8 @@
     <div class="container mx-auto px-4 lg:px-0 pb-8 lg:pt-[110px] lg:pb-14">
         <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
             <div class="relative pb-6 lg:pb-0 h-full flex items-center justify-center reveal slide-left reveal-transition">
-                <div class="lg:w-[400px] lg:h-[370px] overflow-hidden">
-                    <img src="http://localhost/dudunets/wp-content/uploads/2024/07/contact_pattern.svg" class="w-full h-full" />
+                <div class="lg:w-[400px] lg:h-[370px] overflow-hidden hidden lg:block">
+                    <img src="<?php echo get_template_directory_uri()?>/dist/images/contact_pattern.svg" class="w-full h-full" />
                 </div>
             </div>
             <div class="lg:w-11/12 mt-6 reveal slide-right reveal-transition">
@@ -14,21 +14,21 @@
                     <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="name">Name<span class="text-[#c95050]">*</span></label>
-                            <input type="text" name="full_name" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Enter your name" />
+                            <input type="text" name="full_name" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Enter your name" />
                         </div>
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="phone">Phone<span class="text-[#c95050]">*</span></label>
-                            <input type="text" name="mobile" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Enter your phone number" />
+                            <input type="text" name="mobile" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Enter your phone number" />
                         </div>
                     </div>
                     <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="email">Email<span class="text-[#c95050]">*</span></label>
-                            <input type="email" name="email" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Enter your email" />
+                            <input type="email" name="email" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Enter your email" />
                         </div>
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="service">Select Service<span class="text-[#c95050]">*</span></label>
-                            <select class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" name="service">
+                            <select class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" name="service">
                                 <option>Select Service</option>
                                 <option>Service 1</option>
                                 <option>Service 2</option>
@@ -39,17 +39,17 @@
                     <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-5">
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="city">City<span class="text-[#c95050]">*</span></label>
-                            <input type="text" name="location" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Enter your city" />
+                            <input type="text" name="location" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Enter your city" />
                         </div>
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="area">Area<span class="text-[#c95050]">*</span></label>
-                            <input type="text" name ="area" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Enter your area" />
+                            <input type="text" name ="area" class="w-full h-[45px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Enter your area" />
                         </div>
                     </div>
                     <div class="grid grid-cols-1 lg:gap-5">
                         <div class="mb-3">
                             <label class="block mb-2 font-medium" for="message">Message<span class="text-[#c95050]">*</span></label>
-                            <textarea name="message" class="w-full h-[100px] border-[1px] border-black/10 py-2 px-5 rounded-lg text-sm" placeholder="Write comment..."></textarea>
+                            <textarea name="message" class="w-full h-[100px] border-[1px] border-black/10 py-2 px-5 text-sm" placeholder="Write comment..."></textarea>
                         </div>
                     </div>
                     <div class="grid grid-cols-1 lg:gap-10">
