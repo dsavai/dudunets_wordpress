49cb9302d6238efc87b15377d0c350a87b7048e3
denni savai <dennis.savai316@gmail.com>
2025-10-29 09:16:56 +0300

    added files

--- wp-content/themes/dudunets_v2/page-about-us.php
+++ wp-content/themes/dudunets_v2/page-about-us.php
@@ -14,11 +14,11 @@ $the_impact_image = get_post_thumbnail($the_impact_page->ID);
         <section class="module module--banner pt-[30px]">
             <div>
                 <div class="container mx-auto">
-                    <div class="flex items-center">
+                    <div class="lg:flex items-center px-4 lg:px-0">
                         <div class="w-full lg:w-6/12">
-                            <h1 class="text-5xl font-bold leading-12 mb-6"><?php echo $about_us_page->post_excerpt;?></h1>
+                            <h1 class="text-3xl lg:text-5xl font-bold leading-12 mb-3 lg:mb-6"><?php echo $about_us_page->post_excerpt;?></h1>
                             <p><?php echo $about_us_page->post_content;?></p>
-                            <div>
+                            <div class="mb-8 lg:mb-0">
                                 <a href="<?php echo get_field('cta',$about_us_page->ID)['url']?>" class="inline-flex gap-3 items-center mt-6 font-semibold bg-[#0CBD92] p-2 text-white px-8 py-3 hover:bg-secondary hover:text-primary">
                                     <span>Contact us</span>
                                     <span>
@@ -28,54 +28,45 @@ $the_impact_image = get_post_thumbnail($the_impact_page->ID);
                                 </a>
                             </div>
                         </div>
-                        <div class="w-full lg:w-6/12">
-                            <div class="w-full lg:h-[400px] overflow-hidden ml-10">
-                                <img src="<?php echo $image['image'][0]?>" alt="<?php echo $image['alt']?>" class="w-full h-full" />    
+                        <div class="w-full lg:w-6/12 overflow-hidden">
+                            <div class="w-full h-[200px] lg:h-[400px] overflow-hidden lg:ml-10">
+                                <img src="<?php echo $image['image'][0]?>" alt="<?php echo $image['alt']?>" class="w-full h-full object-cover object-center" />    
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="bg-[#f8f8f8] py-16 my-16">
+            <div class="bg-[#f8f8f8] py-10 lg:py-16 my-10 lg:my-16">
                 <div class="container mx-auto">
-                    <h2 class="mt-1 mb-3 text-3xl font-bold"><?php echo $mission_page->post_title;?></h2>
-                    <div class="w-[80%]">
-                        <?php echo apply_filters("the_content",$mission_page->post_content)?>
-                        <!--<p class="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quisquam deleniti, provident sunt id dignissimos molestiae sint temporibus mollitia exercitationem.</p>
-                        <p class="mb-6">Our mission is to provide high-quality products that combine performance with value pricing, while establishing a successful relationship with our customers and our suppliers.</p>-->
-                    </div>
-                    <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10 mt-10">
-
-                        <?php foreach($values_data as $data):?>
-                            <div class="bg-white p-8 border border-gray-light text-center">
-                                <h3 class="mt-2 text-xl font-semibold mb-2"><?php echo $data['field_name']?></h3>
-                                <p class="text-[15px]"><?php echo $data['value']?></p>
-                            </div>
-                        <?php endforeach;?>
-                        <!--<div class="bg-white p-8 border border-gray-light text-center">
-                            <h3 class="mt-2 text-xl font-semibold mb-2">Customer Centricity</h3>
-                            <p class="text-[15px]">We are committed to delivering products that meet the highest standards of quality and reliability.</p>
+                    <div class="px-4 lg:px-0">
+                        <h2 class="mt-1 mb-3 text-3xl font-bold"><?php echo $mission_page->post_title;?></h2>
+                        <div class="lg:w-[80%] about-content-container">
+                            <?php echo apply_filters("the_content",$mission_page->post_content)?>
                         </div>
-                        <div class="bg-white p-8 border border-gray-light text-center">
-                            <h3 class="mt-2 text-xl font-semibold mb-2">Accountability</h3>
-                            <p class="text-[15px]">We are committed to delivering products that meet the highest standards of quality and reliability.</p>
+                        <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10 mt-10">
+                            <?php foreach($values_data as $data):?>
+                                <div class="bg-white p-8 border border-gray-light text-center mb-3 lg:mb-0">
+                                    <div class="flex justify-center">
+                                        <svg class="w-8 h-8 fill-current">
+                                            <use xlink:href="#icon-tick-circle"></use>
+                                        </svg>
+                                    </div>
+                                    <h3 class="mt-2 text-xl font-semibold mb-2"><?php echo $data['field_name']?></h3>
+                                    <p class="text-[15px]"><?php echo $data['value']?></p>
+                                </div>
+                            <?php endforeach;?>
                         </div>
-                        <div class="bg-white p-8 border border-gray-light text-center">
-                            <h3 class="mt-2 text-xl font-semibold mb-2">Innovation</h3>
-                            <p class="text-[15px]">We are committed to delivering products that meet the highest standards of quality and reliability.</p>
-                        </div>-->
                     </div>
                 </div>
             </div>
             <div>
                 <div class="container mx-auto">
-                    <h2 class="mt-1 mb-10 text-3xl font-bold">Our Management Team</h2>
-                    <div class="grid grid-cols-1 lg:grid-cols-4 lg:gap-10">
-
-                        <?php foreach ($team_members as $team_member):
-                            $image = get_post_thumbnail($team_member->ID);
-                            ?>
-
+                    <div class="px-4 lg:px-0">
+                        <h2 class="mt-1 mb-10 text-3xl font-bold">Our Management Team</h2>
+                        <div class="grid grid-cols-1 lg:grid-cols-4 lg:gap-10">
+                            <?php foreach ($team_members as $team_member):
+                                $image = get_post_thumbnail($team_member->ID);
+                                ?>
                                 <div class="mb-6">
                                     <div>
                                         <img src="<?php echo $image['image'][0]?>" alt="<?php echo $image['alt']?>" />
@@ -83,37 +74,47 @@ $the_impact_image = get_post_thumbnail($the_impact_page->ID);
                                         <p class="text-black/60 text-sm"><?php echo get_field("designation",$team_member->ID)?></p>
                                     </div>
                                 </div>
-                        <?php endforeach;?>
+                            <?php endforeach;?>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="bg-[#f8f8f8] py-16 my-16">
+            <div class="bg-[#f8f8f8] py-10 lg:py-16 my-10 lg:my-16">
                 <div class="container mx-auto">
-                    <h2 class="mt-1 mb-3 text-3xl font-bold"><?php echo $why_choose_us_page->post_title?></h2>
-                    <div class="w-[80%]">
-                       <?php echo apply_filters("the_content",$why_choose_us_page->post_content)?>
-                    </div>
-                    <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10 mt-10">
-                       <?php foreach($why_choose_us_page_values_data as $data):?>
-                            <div class="bg-white p-8 border border-gray-light text-center">
-                                <h3 class="mt-2 text-xl font-semibold mb-2"><?php echo $data['field_name']?></h3>
-                                <p class="text-[15px]"><?php echo $data['value']?></p>
-                            </div>
-                        <?php endforeach;?>
+                    <div class="px-4 lg:px-0">
+                        <h2 class="mt-1 mb-3 text-3xl font-bold"><?php echo $why_choose_us_page->post_title?></h2>
+                        <div class="lg:w-[80%] about-content-container">
+                           <?php echo apply_filters("the_content",$why_choose_us_page->post_content)?>
+                        </div>
+                        <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10 mt-10">
+                           <?php foreach($why_choose_us_page_values_data as $data):?>
+                                <div class="bg-white p-8 border border-gray-light text-center mb-3 lg:mb-0">
+                                    <div class="flex justify-center">
+                                        <svg class="w-8 h-8 fill-current">
+                                            <use xlink:href="#icon-tick-circle"></use>
+                                        </svg>
+                                    </div>
+                                    <h3 class="mt-2 text-xl font-semibold mb-2"><?php echo $data['field_name']?></h3>
+                                    <p class="text-[15px]"><?php echo $data['value']?></p>
+                                </div>
+                            <?php endforeach;?>
+                        </div>
                     </div>
                 </div>
             </div>
             <div>
                 <div class="container mx-auto">
-                    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
-                        <div>
-                            <h2 class="mt-1 mb-3 text-3xl font-bold"><?php $the_impact_page->post_title;?></h2>
-                            <div class="w-[80%]">
-                                <?php echo apply_filters("the_content",$the_impact_page->post_content)?>   
+                    <div class="px-4 lg:px-0">
+                        <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
+                            <div>
+                                <h2 class="mt-1 mb-3 text-3xl font-bold"><?php $the_impact_page->post_title;?></h2>
+                                <div class="lg:w-[80%] about-content-container">
+                                    <?php echo apply_filters("the_content",$the_impact_page->post_content)?>   
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <img src="<?php echo $the_impact_image['image'][0]?>" alt="<?php echo $the_impact_image['alt']?>" />
+                            <div>
+                                <img src="<?php echo $the_impact_image['image'][0]?>" alt="<?php echo $the_impact_image['alt']?>" />
+                            </div>
                         </div>
                     </div>
                 </div>
